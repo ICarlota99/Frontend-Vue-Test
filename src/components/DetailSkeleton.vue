c98ce495baf5017801dfa0a3feac11a302e5c02c
ICarlota99 <k431999@gmail.com>
2025-04-15 15:14:20 -0400

    fix: separate skeletons into different components

---
+++ src/components/DetailSkeleton.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="space-y-4 animate-pulse px-64">
+        <!-- Skeleton Loader -->
+        <div class="flex space-x-6 animate-pulse flex-col lg:flex-row gap-6 p-6 lg:px-12 xl:px-20 2xl:px-24 max-w-6xl mx-auto">
+            <!-- Skeleton for Product Image -->
+            <div class="w-full object-contain bg-gray-300 rounded-lg"></div>
+
+            <!-- Skeleton for Product Content (name, price, quantity) -->
+            <div class="flex-1 space-y-4">
+                <!-- Skeleton for Product Name -->
+                <div class="w-2/5 h-4 bg-gray-300 rounded-md"></div>
+                <div class="flex-1 bg-gray-300 h-15 w-3/4 rounded-md"></div>
+
+                <!-- Skeleton for Prices -->
+                <div class="w-1/3 h-5 bg-gray-300 rounded-md"></div>
+                <div class="w-1/3 h-3 bg-gray-300 rounded-md"></div>
+                <div class="w-1/3 h-3 bg-gray-300 rounded-md"></div>
+
+                <!-- Skeleton for Quantity Selector -->
+                <div class="flex items-center gap-2 mt-4 pt-2 border-t border-gray-100">
+                <div class="w-1/10 h-3 bg-gray-300 rounded-md"></div>
+                <div class="flex items-center border rounded-md overflow-hidden text-sm">
+                    <div class="w-6 h-8 bg-gray-300"></div>
+                    <div class="w-10 h-8 bg-gray-300 mx-2"></div>
+                    <div class="w-6 h-8 bg-gray-300"></div>
+                </div>
+                </div>
+                <!-- Skeleton for buttons -->
+                <div class="flex gap-2 mt-4 animate-pulse">
+                <div class="bg-gray-200 h-8 w-32 rounded"></div>
+                <div class="bg-gray-200 h-8 w-32 rounded"></div>
+                </div>
+                <!-- Text  -->
+                <div class="w-1/3 h-3 bg-gray-300 rounded-md"></div>
+                <div class="w-1/3 h-3 bg-gray-300 rounded-md"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
---
+++ src/components/skeletons/CardSkeleton.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+<template>
+    <!-- Skeleton Loading State -->
+    <div class="bg-white rounded-xl border border-gray-200 shadow-sm p-3 flex flex-row md:flex-col gap-4 h-full relative">
+      <!-- Skeleton for Product Image -->
+      <div class="w-full aspect-square mb-3 relative bg-gray-200 rounded-lg animate-pulse"></div>
+
+      <!-- Skeleton for Product Content -->
+      <div class="flex flex-col flex-1">
+        <div class="h-4 bg-gray-200 rounded-full animate-pulse mb-2 w-3/4"></div>
+        <div class="h-4 bg-gray-200 rounded-full animate-pulse mb-4 w-1/2"></div>
+
+        <!-- Price Skeleton -->
+        <div class="flex items-center gap-2 mt-1">
+          <div class="h-6 bg-gray-200 rounded-full animate-pulse w-16"></div>
+          <div class="h-4 bg-gray-200 rounded-full animate-pulse w-12"></div>
+          <div class="ml-auto h-5 bg-gray-200 rounded-full animate-pulse w-20"></div>
+        </div>
+
+        <!-- Quantity Selector + Add to Cart Skeleton -->
+        <div class="flex items-center gap-2 mt-4 pt-2 border-t border-gray-100">
+          <div class="flex items-center border rounded-md text-sm w-24 h-8 bg-gray-200 animate-pulse"></div>
+          <div class="flex-1 h-8 bg-gray-200 rounded-md animate-pulse"></div>
+        </div>
+      </div>
+    </div>
+</template>
