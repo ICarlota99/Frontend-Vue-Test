ed3edd600552d66011a1a14b224a3fb386211fc2
ICarlota99 <k431999@gmail.com>
2025-04-15 23:03:33 -0400

    fix: separate all products link into a new component

---
+++ src/components/AllProductsLink.vue
@@ -0,0 +1,6 @@
+<template>
+    <a href="" class="block mx-auto w-fit py-2 hover:bg-gray-400 p-2 rounded-md font-semibold lg:text-2xl md:text-xl sm:text-lg mt-6">
+      Ver todos los productos 
+      <i class="pi pi-arrow-right bg-gray-200 rounded-full p-2"></i>
+    </a>
+</template>
