c816dff9d2503301f2065f7c5fc3f7ea4f324391
ICarlota99 <k431999@gmail.com>
2025-04-16 08:16:31 -0400

    feat(skeleton): add round image skeleton

---
+++ src/components/skeletons/RoundImageSkeleton.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex flex-col items-center gap-2 p-2">
+        <!-- Skeleton Image -->
+        <div class="w-20 h-20 lg:w-60 lg:h-60 md:w-40 md:h-40">
+            <div class="w-full h-full bg-gray-300 animate-pulse rounded-full border border-gray-600"></div>
+        </div>
+
+        <!-- Skeleton Text -->
+        <div class="w-32 h-6 bg-gray-300 animate-pulse rounded-md"></div>
+    </div>
+</template>
