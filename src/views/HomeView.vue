f3005c801bf55ae08770d159cfd6ef70b1ca1766
ICarlota99 <k431999@gmail.com>
2025-04-16 11:01:36 -0400

    add skeleton banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { useProductStore } from '@/store/productStore'
-import { useCategoryStore } from '@/store/categoryStore'
 import { onMounted } from 'vue'
 
 import Hero from '@/components/home_sections/Hero.vue'
@@ -10,27 +9,27 @@ import Banner from '@/components/home_sections/Banner.vue'
 import Whyus from '@/components/home_sections/Whyus.vue'
 import SuscriptionBanner from '@/components/home_sections/SuscriptionBanner.vue'
 // import Contact from '@/components/home_sections/Contact.vue'
+import BannerSkeleton from '@/components/skeletons/BannerSkeleton.vue'
 
 const store = useProductStore()
-const catStore = useCategoryStore()
 
 onMounted(() => {
   store.fetchProducts()
-  catStore.fetchCategories()
 })
 </script>
 
 <template>
-  <Hero />
-  <ScrollingGrid title="" :isCategoryView="true" :categories="catStore.categories" :products="[]"/>
+  <Hero v-if="!store.productsLoading" />
+  <BannerSkeleton v-else />
+  <ScrollingGrid title="" :isCategoryView="true" :products="[]"/>
   <ProductGrid 
     title="Productos recomendados" 
     :products="store.products" 
     class="lg:px-10 xl:px-32 py-6 bg-gray-100" 
   /> 
   <Banner />
-  <ProductGrid title="Productos más vendidos" :products="store.products" class="lg:px-10 xl:px-32 py-6 bg-white" />
-  <ScrollingGrid title="Últimos añadidos" :isCategoryView="false" :categories="[]" :products="store.products" class="bg-gray-100 py-16"/>
+  <ProductGrid title="Productos más vendidos" :isCategoryView="false" :products="store.products" class="lg:px-10 xl:px-32 py-6 bg-white" />
+  <ScrollingGrid title="Últimos añadidos" :isCategoryView="false" :products="store.products" class="bg-gray-100 py-16"/>
   <Whyus />
   <SuscriptionBanner />
   <!-- <ScrollingGrid title="Vinculados a tus compras" /> -->
