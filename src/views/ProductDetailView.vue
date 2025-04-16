fda7b80751e106f1780b3cea287ca75feb93d817
ICarlota99 <k431999@gmail.com>
2025-04-16 00:36:37 -0400

    add scrolling gris to product detail view

--- src/views/ProductDetailView.vue
+++ src/views/ProductDetailView.vue
@@ -4,6 +4,7 @@ import { onMounted } from 'vue'
 import { useProductStore } from '@/store/productStore'
 import ProductDetail from '@/components/ProductDetail.vue'
 import DetailSkeleton from '@/components/skeletons/DetailSkeleton.vue'
+import ScrollingGrid from '@/components/ScrollingGrid.vue'
 
 const route = useRoute()
 const store = useProductStore()
@@ -17,10 +18,12 @@ onMounted(async () => {
   <div class="p-6 xl:px-32 bg-gray-50">
     <ProductDetail 
       v-if="store.productDetails" 
+      :key="store.productDetails.id"
       :product="store.productDetails" 
     />
     <div v-else class="text-center py-10">
       <DetailSkeleton />
     </div>
   </div>
+  <ScrollingGrid title="Productos similares" :isCategoryView="false" :categories="[]" :products="store.products" class="bg-gray-100 py-16"/>
 </template>
