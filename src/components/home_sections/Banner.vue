4a9358a54778b607c5506fd156f5bb6cc803d3b6
ICarlota99 <k431999@gmail.com>
2025-04-15 23:02:33 -0400

    feat(middle banner): add middle banner in homepage

---
+++ src/components/home_sections/Banner.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import banner from "/banner_middle.png";
+</script>
+
+<template>
+      <div class="relative w-full overflow-hidden">
+        <div class="flex w-full h-auto transition-transform duration-500">
+            <!-- Banner -->
+                <img
+                :src="banner"
+                class="w-full h-full object-cover"
+                alt="banner"
+                />
+        </div>
+      </div>
+</template>
