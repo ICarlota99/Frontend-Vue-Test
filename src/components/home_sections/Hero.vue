37d26ea81fea0a4488a4ed2b591d2ccb5a3c2fb1
ICarlota99 <k431999@gmail.com>
2025-06-11 09:27:31 -0400

    fix: improve hero banner with keen-slider

--- src/components/home_sections/Hero.vue
+++ src/components/home_sections/Hero.vue
@@ -1,53 +1,123 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
+import { useKeenSlider } from 'keen-slider/vue';
+import 'keen-slider/keen-slider.min.css';
+
 import banner1 from "/banner1.png";
 import banner2 from "/banner2.png";
 
 const banners = [banner1, banner2];
-const currentIndex = ref(0);
+const current = ref(0);
+const [container, slider] = useKeenSlider({
+  initial: current.value,
+  slideChanged: (s) => {
+    current.value = s.track.details.rel;
+  },
+});
 
-const prevBanner = () => {
-  currentIndex.value = (currentIndex.value - 1 + banners.length) % banners.length;
-};
-
-const nextBanner = () => {
-  currentIndex.value = (currentIndex.value + 1) % banners.length;
-};
+const dotHelper = computed(() => 
+  slider.value ? [...Array(slider.value.track.details.slides.length).keys()] : []
+);
 </script>
 
 <template>
-  <div class="relative w-full overflow-hidden">
-    <!-- Container for banners -->
-    <div class="flex w-full h-auto transition-transform duration-500">
-      <!-- Banners as images -->
+  <div class="navigation-wrapper">
+    <!-- Slider Container -->
+    <div ref="container" class="keen-slider">
+      <!-- Slides -->
       <div 
         v-for="(banner, index) in banners"
         :key="index"
-        class="w-full flex-shrink-0 transition-transform duration-500"
-        :style="{
-          transform: `translateX(${-100 * currentIndex}%)`
-        }"
+        class="keen-slider__slide"
       >
-        <img
-          :src="banner"
-          class="w-full h-full object-cover"
-          alt="banner"
-        />
+        <img :src="banner" alt="banner" />
       </div>
     </div>
 
-    <!-- Navigation arrows -->
-    <button
-      @click="prevBanner"
-      class="absolute left-4 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-25 p-2 rounded-full hover:bg-opacity-75 transition-all z-10"
+    <!-- Navigation Arrows -->
+    <svg
+      v-if="slider"
+      @click="slider.prev()"
+      :class="{
+        'arrow': true,
+        'arrow--left': true,
+        'arrow--disabled': current === 0,
+      }"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
     >
-      &lt;
-    </button>
-    <button
-      @click="nextBanner"
-      class="absolute right-4 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-25 p-2 rounded-full hover:bg-opacity-75 transition-all z-10"
+      <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/>
+    </svg>
+    
+    <svg
+      v-if="slider"
+      @click="slider.next()"
+      :class="{
+        'arrow': true,
+        'arrow--right': true,
+        'arrow--disabled': current === slider.track.details.slides.length - 1,
+      }"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
     >
-      &gt;
-    </button>
+      <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/>
+    </svg>
+
+    <!-- Dots Navigation -->
+    <div v-if="slider" class="dots">
+      <button
+        v-for="(_, idx) in dotHelper"
+        @click="slider.moveToIdx(idx)"
+        :class="{ 'dot': true, 'active': current === idx }"
+        :key="idx"
+      ></button>
+    </div>
   </div>
 </template>
+
+<style>
+  .navigation-wrapper {
+    position: relative;
+  }
+  .dots {
+    display: flex;
+    padding: 10px 0;
+    justify-content: center;
+  }
+  .dot {
+    border: none;
+    width: 10px;
+    height: 10px;
+    background: #c5c5c5;
+    border-radius: 50%;
+    margin: 0 5px;
+    padding: 5px;
+    cursor: pointer;
+  }
+  .dot:focus {
+    outline: none;
+  }
+  .dot.active {
+    background: #000;
+  }
+  .arrow {
+    width: 30px;
+    height: 30px;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    -webkit-transform: translateY(-50%);
+    fill: #fff;
+    cursor: pointer;
+  }
+  .arrow--left {
+    left: 5px;
+  }
+  .arrow--right {
+    left: auto;
+    right: 5px;
+  }
+  .arrow--disabled {
+    fill: rgba(255, 255, 255, 0.5);
+  }
+</style>
