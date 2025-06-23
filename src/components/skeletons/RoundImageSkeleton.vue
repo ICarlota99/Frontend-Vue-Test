fb6b60d8c4a25061d96977b6bc4d15d01a855b3b
ICarlota99 <k431999@gmail.com>
2025-06-23 06:44:07 -0400

    fix: size of skeleton image

--- src/components/skeletons/RoundImageSkeleton.vue
+++ src/components/skeletons/RoundImageSkeleton.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="w-32 md:w-36 lg:w-40 flex flex-col items-center gap-2 p-2 animate-pulse">
+  <div class="w-16 h-32 md:w-36 md:h-46 lg:w-60 lg:h-60 flex flex-col items-center gap-4 p-2 animate-pulse">
     <!-- Image -->
-    <div class="w-24 h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 rounded-full bg-gray-300 dark:bg-gray-700 border border-gray-400" />
+    <div class="w-16 h-16 md:w-28 md:h-28 lg:w-40 lg:h-40 rounded-full bg-gray-300 dark:bg-gray-700 border border-gray-400" />
     
     <!-- Label -->
     <div class="h-4 w-20 md:w-24 lg:w-28 bg-gray-300 dark:bg-gray-700 rounded" />
